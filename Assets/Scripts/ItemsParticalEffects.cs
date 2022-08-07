63218d3638c76f550119a8762599b5cec2aa3131
ShuxuanCai <caishuxuan00@gmail.com>
2022-08-06 17:46:14 -0700

    AnimalWorld

---
+++ Assets/Scripts/ItemsParticalEffects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsParticalEffects : MonoBehaviour {
+
+	private float radian = 0.0f;
+	private float perRad = 0.03f;
+	private float dis = 0.0f;
+	private Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		radian += perRad;
+		dis = Mathf.Sin(radian);
+		transform.position = pos + new Vector3(0.0f, dis, 0.0f);
+
+		transform.Rotate(0.0f, Time.deltaTime + 2.5f, 0.0f, Space.World);
+	}
+}
---
+++ Assets/Scripts/SpawnPoints.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour {
+
+	public GameObject Cage;
+	public GameObject Animals;
+	public GameObject Food;
+
+	// Use this for initialization
+	void Start () {
+		int randomEvent = Random.Range(0, 3);
+		if(randomEvent == 0)
+        {
+			SpawnAnimals();
+        }
+		else if(randomEvent == 1)
+        {
+			SpawnCages();
+        }
+		else
+        {
+			SpawnFood();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SpawnAnimals()
+    {
+		Instantiate(Animals, transform.position, transform.rotation);
+    }
+
+	private void SpawnCages()
+	{
+		Quaternion q = new Quaternion(180.0f, 0.0f, 0.0f, 0.0f);
+		Instantiate(Cage, transform.position + new Vector3(0.0f, 3.0f, 0.0f), q);
+	}
+
+	private void SpawnFood()
+	{
+		Instantiate(Food, transform.position + new Vector3(0.0f, 3.0f, 0.0f), transform.rotation);
+	}
+}
